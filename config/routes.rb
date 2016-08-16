39a636cf9427f7f462f979a65498c17594f372bd
Dakota A. Day <kaineunbound@gmail.com>
2016-08-16 16:04:10 -0700

    added links to each page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get "about" =>"pages#about"
 
 get "contact" => "pages#contact"
 
-
+get "home" => "pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
